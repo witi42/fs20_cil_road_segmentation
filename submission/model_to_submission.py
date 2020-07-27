5ca9fe0d5204dfa8353ccb325d92f43324f869a0
robinw <robinw@student.ethz.ch>
2020-07-28 01:03:39 +0200

    split submission by putting large image together from 4 smaller predictions images

--- submission/model_to_submission.py
+++ submission/model_to_submission.py
@@ -28,3 +28,48 @@ def create(model, sub_name):
     image_filenames = glob.glob('output/*.png')
 
     mask_to_submission.masks_to_submission(submission_filename, image_filenames)
+
+
+def create_with_split(model, sub_name):
+    small_s = 400
+    large_s = 608
+
+    x, x_names = data.get_test_data()
+
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    for i in range(len(x_names)):
+        name = x_names[i][18:]
+        print(name)
+
+        image = x[i:i + 1][0]
+        print(image.shape)
+        aa = image[0:small_s, 0:small_s]
+        ab = image [0:small_s, large_s-small_s: large_s]
+        ba = image [large_s-small_s: large_s, 0:small_s]
+        bb = image [large_s-small_s: large_s, large_s-small_s: large_s]
+
+        x_splits = np.array([aa, ab, ba, bb])
+
+        pred = model.predict(x_splits)
+        pred = pred.reshape(4, small_s, small_s)
+        pred = (pred > 0.5).astype(np.uint8)
+
+        aa_ = pred[0, 0:large_s // 2, 0:large_s // 2]
+        ab_ = pred[1, 0:large_s // 2, small_s - large_s // 2:small_s]
+        ba_ = pred[2, small_s - large_s // 2:small_s, 0:large_s // 2]
+        bb_ = pred[3, small_s - large_s // 2:small_s, small_s - large_s // 2:small_s]
+
+        a = np.concatenate((aa_, ab_), axis=1)
+        b = np.concatenate((ba_, bb_), axis=1)
+        out = np.concatenate((a, b), axis=0)
+
+        plt.imsave("output/" + name, out, cmap=cm.gray)
+
+    if not os.path.exists('submission_csv'):
+        os.makedirs('submission_csv')
+    submission_filename = 'submission_csv/' + sub_name + '.csv'
+    image_filenames = glob.glob('output/*.png')
+
+    mask_to_submission.masks_to_submission(submission_filename, image_filenames)
