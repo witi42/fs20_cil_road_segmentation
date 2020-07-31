25495ed677d9b3f7dc785c7d1ad3c888d60691e7
robinw <robinw@lo-login-01.leonhard.ethz.ch>
2020-07-31 16:42:18 +0200

    novel

--- train_for_submission_novel_dice.py
+++ train_for_submission_novel_dice.py
@@ -44,29 +44,26 @@ def train_sub(model, model_name, train, val, epochs=100, batch_size=8, verbose=2
 def main():
 
     from models.sdf_model import get_CNN_SDFt, get_flat_tanh_CNN_SDFt
+    from losses.sdf_adapter import convert_to_sdft_loss
+    from losses.dice import dice_loss
     
-
-    # # no augmentation
-
-    # # sdf flat tanh
-    # train, val = data_g.get_train_val_iterators(aug=None)
-
-    # model = get_flat_tanh_CNN_SDFt()
-    # model_name = 'SDF_cnn_scaled_tanh_EXTDATA_noaug'
-
-    # train_sub(model, model_name, train, val, epochs=100, verbose=1)
-    # submission.create(model, model_name)
-
-
-    # sdf normal tanh
-
     train, val = data_g.get_train_val_iterators(aug=None)
 
-    model = get_CNN_SDFt()
-    model_name = 'SDF_cnn_tanh_EXTDATA_noaug'
+
+    msebce = lambda x, y: tf.keras.losses.MSE(x, y) + convert_to_sdft_loss(tf.keras.losses.binary_crossentropy)(x, y)
+    msedice = lambda x, y: tf.keras.losses.MSE(x, y) + convert_to_sdft_loss(dice_loss)(x, y)
+
+    # model = get_CNN_SDFt(loss=msedice)
+    # model_name = 'SDF-tanh_msedice_loss'
+
+    # train_sub(model, model_name, train, val, epochs=100, verbose=1)
+
+
+    model = get_flat_tanh_CNN_SDFt(loss=msedice)
+    model_name = 'SDF-flat_tanh_msedice_loss'
 
     train_sub(model, model_name, train, val, epochs=100, verbose=1)
-    submission.create(model, model_name)
+
 
     # for _aug in ['small', 'medium', 'large']:
     #     train, val = data_g.get_train_val_iterators(aug=_aug)
